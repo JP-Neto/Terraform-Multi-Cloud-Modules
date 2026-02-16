4629e57bf31b3213dccf30a0084278d1cdf97048
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-16 15:11:08 -0300

    feat: outputs VCN Security list

--- modules/oci/network/vcn/outputs.tf
+++ modules/oci/network/vcn/outputs.tf
@@ -7,3 +7,7 @@ output "vcn_cidr" {
   description = "Bloco CIDR da VCN"
   value       = oci_core_vcn.this.cidr_block
 }
+
+output "default_security_list_id" {
+  value = oci_core_vcn.this.default_security_list_id
+}
