b3dc2af1489a09262399a4560d77df9fc87f910c
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-16 14:40:35 -0300

    fix: route table search services

--- modules/oci/network/route-table/main.tf
+++ modules/oci/network/route-table/main.tf
@@ -1,4 +1,4 @@
-resource "oci_core_route_table" "this" {
+/*resource "oci_core_route_table" "this" {
   compartment_id = var.compartment_id
   vcn_id         = var.vcn_id
   display_name   = var.display_name
@@ -10,6 +10,33 @@ resource "oci_core_route_table" "this" {
       destination       = route_rules.value.destination           
       destination_type  = can(regex("services", route_rules.value.destination)) ? "SERVICE_CIDR_BLOCK" : "CIDR_BLOCK"      
       description       = lookup(route_rules.value, "description", null)
+    }
+  }
+
+  freeform_tags = var.tags
+}
+*/
+# Busca dinamicamente os serviços da região atual (ex: Vinhedo)
+data "oci_core_services" "this" {
+  filter {
+    name   = "name"
+    values = ["All .* Services In Oracle Services Network"]
+    regex  = true
+  }
+}
+
+resource "oci_core_route_table" "this" {
+  compartment_id = var.compartment_id
+  vcn_id         = var.vcn_id
+  display_name   = var.display_name
+
+  dynamic "route_rules" {
+    for_each = var.route_rules
+    content {
+      network_entity_id = route_rules.value.network_entity_id
+      description       = lookup(route_rules.value, "description", null)      
+      destination_type  = lookup(route_rules.value, "destination_type", "CIDR_BLOCK")
+      destination       = lookup(route_rules.value, "destination_type", "CIDR_BLOCK") == "SERVICE_CIDR_BLOCK" ? data.oci_core_services.this.services[0].cidr_block : route_rules.value.destination
     }
   }
 
