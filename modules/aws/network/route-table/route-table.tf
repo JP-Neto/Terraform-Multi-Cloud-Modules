91f1ced10ca681bfad76ef8547b525372f466783
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-19 23:12:05 -0300

    Update route-table.tf

--- modules/aws/network/route-table/route-table.tf
+++ modules/aws/network/route-table/route-table.tf
@@ -1,7 +1,5 @@
 resource "aws_route_table" "this" {
   count  = length(var.subnet_ids)
   vpc_id = var.vpc_id
-  tags   = {
-    Name = "RouteTable-${[var.azs[count.index]]}-${var.azs[count.index]}"
-  }
+  tags   = var.tags
 }
