00b4feb970f8d81968a600173ca6c86667484451
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-19 23:09:51 -0300

    fix route table

--- modules/aws/network/route-table/route-table.tf
+++ modules/aws/network/route-table/route-table.tf
@@ -2,6 +2,6 @@ resource "aws_route_table" "this" {
   count  = length(var.subnet_ids)
   vpc_id = var.vpc_id
   tags   = {
-    Name = "RouteTable-${var.service_name[var.azs[count.index]]}-${var.azs[count.index]}"
+    Name = "RouteTable-${[var.azs[count.index]]}-${var.azs[count.index]}"
   }
 }
--- modules/aws/network/route-table/variables.tf
+++ modules/aws/network/route-table/variables.tf
@@ -11,10 +11,6 @@ variable "tags" {
   type = map(string)
 }
 
-variable "service_name" {
-  type = map(string)
-}
-
 variable "azs" {
   type    = list(string)
   default = ["us-east-1a", "us-east-1b","us-east-1c"]
