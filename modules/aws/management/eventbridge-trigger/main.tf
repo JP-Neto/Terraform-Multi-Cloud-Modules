293312af51f78df94cbba05817d0da4f6d1f9717
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-22 11:32:58 -0300

    fix: Event Pattern event brigde

--- modules/aws/management/eventbridge-trigger/main.tf
+++ modules/aws/management/eventbridge-trigger/main.tf
@@ -1,13 +1,14 @@
 resource "aws_cloudwatch_event_rule" "this" {
   name        = var.rule_name
   description = var.description
+
   event_pattern = jsonencode({
-    source      = ["aws.ecr"]
-    detail_type = ["ECR Image Action"]
+    source = ["aws.ecr"]
+    "detail-type" = ["ECR Image Action"]
     detail = {
-      action_type     = ["PutImage"]
-      result          = ["SUCCESS"]
-      repository-name = [var.repository_name]
+      "action-type"     = ["PUSH"]
+      result            = ["SUCCESS"]
+      "repository-name" = [var.repository_name]
     }
   })
 
