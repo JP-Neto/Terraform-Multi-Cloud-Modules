1488c9cf5184b715d30160f3e1378856250d2542
Jose Neto <113240030+JP-Neto@users.noreply.github.com>
2026-02-21 23:31:09 -0300

    Update variables.tf

--- modules/aws/compute/lambda/variables.tf
+++ modules/aws/compute/lambda/variables.tf
@@ -1,7 +1,4 @@
 variable "function_name" { type = string }
 variable "role_arn"      { type = string }
-variable "source_file"   { type = string } # Caminho do arquivo .py
-variable "sns_topic_arn" { type = string }
-variable "cluster_name"  { type = string }
-variable "service_name"  { type = string }
+variable "source_file"   { type = string } 
 variable "tags"          { type = map(string) }
